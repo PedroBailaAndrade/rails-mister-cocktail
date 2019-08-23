61c17e9114370e6a04a4c546ce39656c96c4b37d
Pedro Andrade <pedrocbva@gmail.com>
2019-08-23 12:27:59 +0100

    app start

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,21 @@
 require 'json'
 require 'open-uri'
 
+Ingredient.destroy_all
+Dose.destroy_all
+
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 user_serialized = open(url).read
-@ingredients = JSON.parse(user_serialized)
+ingredients = JSON.parse(user_serialized)
 ingredients["drinks"].each do |ingredient|
   Ingredient.create!(name: ingredient["strIngredient1"])
 end
+
+dose45 = Dose.new(description: '10ml')
+ingredient1 = Ingredient.first
+cocktail1 = Cocktail.last
+dose45.ingredient_id = ingredient1.id
+dose45.cocktail_id = cocktail1.id
+dose45.save
+
 # puts ingredient = JSON.parse(user_serialized["drinks"]["strIngredient1"])
